5447efba20b13acf5eb3a766db9e7902db10570f
ProjetoIntegrador <mjoao8219@gmail.com>
2025-01-20 15:32:34 -0300

    updates

---
+++ app/src/main/java/com/example/testeprojetao/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.testeprojetao;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+public class MainActivity extends AppCompatActivity {
+    Button troca1;
+    Intent tela1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        troca1 = findViewById(R.id.troca1);
+        tela1 = new Intent(MainActivity.this, MainScreen.class);
+        troca1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(tela1);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/testeprojetao/MainScreen.java
@@ -0,0 +1,59 @@
+package com.example.testeprojetao;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+public class MainScreen extends AppCompatActivity {
+    Button trocaquadrado,trocaretangulo,trocacirculo,trocaTrapezio,trocatriangulo;
+    Intent TelaQuadrado,TelaRetangulo,TelaCirculo,TelaTrapezio,TelaTriangulo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        trocaquadrado = findViewById(R.id.trocaquadrado);
+        TelaQuadrado = new Intent(MainScreen.this, TelaQuadrado.class);
+        trocaquadrado.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(TelaQuadrado);
+            }
+        });
+
+        trocaretangulo = findViewById(R.id.trocaretangulo);
+        TelaRetangulo = new Intent(MainScreen.this, TelaRetangulo.class);
+        trocaretangulo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(TelaRetangulo);
+            }
+        });
+
+        trocacirculo = findViewById(R.id.trocacirculo);
+        TelaCirculo = new Intent(MainScreen.this,
+                TelaCirculo.class);
+        trocacirculo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(TelaCirculo);
+            }
+        });
+
+
+
+    }
+}
